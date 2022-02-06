8e466f21e2acd0da825078c51c080d3dd835016f
navI-kryabik <sergei30062003@gmail.com>
2022-02-07 01:35:22 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,1 +1,43 @@
 #include "vector.h"
+#include <malloc.h>
+#include <stdlib.h>
+
+vector createVector(size_t n) {
+    int *data = (int *) malloc(sizeof(int) * n);
+    if (data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    } else
+        return (vector) {data, 0, n};
+}
+
+void reserve(vector *v, size_t newCapacity) {
+    v->capacity = newCapacity;
+    if (v->data == NULL) {
+        v->data = (int *) realloc(v->data, sizeof(int) * newCapacity);
+        v->data = NULL;
+    }
+    if (v->data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    } else if (newCapacity == 0)
+        v->data = NULL;
+    else if (newCapacity < v->size)
+        v->size = newCapacity;
+
+}
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    v->capacity = v->size;
+}
+
+void deleteVector(vector *v) {
+    if (v->data == NULL) {
+        free(v->data);
+        v->data = NULL;
+    }
+}
--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,4 +1,22 @@
 #ifndef INC_5B_VECTOR_H
 #define INC_5B_VECTOR_H
 
+#include <stdio.h>
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+vector createVector(size_t n);
+
+void reserve(vector *v, size_t newCapacity);
+
+void clear(vector *v);
+
+void shrinkToFit(vector *v);
+
+void deleteVector(vector *v);
+
 #endif
