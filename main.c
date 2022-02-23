60021a5b236394a68e0a540ef3672516f4b6177d
navI-kryabik <sergei30062003@gmail.com>
2022-02-23 21:17:26 +0300

    exercise 13

--- main.c
+++ main.c
@@ -619,7 +619,26 @@ void swapPenultimateRow(matrix m) {
 }
 
 // Задание 13
+bool isNonDescendingSorted(const int *a, int n) {
+    for (size_t i = 1; i < n; i++)
+        if (a[i] < a[i - 1])
+            return false;
+    return true;
+}
 
+bool hasAllNonDescendingRows(matrix m) {
+    for (size_t i = 0; i < m.nRows; i++)
+        if (!isNonDescendingSorted(m.values[i], m.nCols))
+            return false;
+    return true;
+}
+
+int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
+    int count = 0;
+    for (size_t i = 0; i < nMatrix; i++)
+        count += hasAllNonDescendingRows(ms[i]);
+    return count;
+}
 
 
 int main() {
